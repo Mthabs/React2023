55b8dc2338b95a33672a6686896088d572148c5d
Mthabs <>
2023-11-10 11:59:32 +0000

    add rendering lists

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,6 +6,7 @@ import ConditionalRenderingFunctional from './components/ConditionalRenderingFun
 import EventsClass from './components/EventsClass';
 import EventsFunctional from './components/EventsFunctional';
 import FunctionalGreetingWithProps from "./components/FunctionalGreetingWithProps";
+import RenderingLists from "./components/RenderingLists";
 import StatefulGreetingBindingBindThis from './components/StatefulGreetingBindingBindThis';
 import StatefulGreetingBindingConstructor from './components/StatefulGreetingBindingConstructor';
 import StatefulGreetingBindingDefine from './components/StatefulGreetingBindingDefine';
@@ -20,6 +21,7 @@ function App() {
             <EventsClass />
             <EventsFunctional />
             <FunctionalGreetingWithProps greeting="Nice to meet you!!!" name="Mike!" age="32" />
+            <RenderingLists/>
             <StatefulGreetingBindingBindThis name="John!" greeting="How are you doing?" />
             <StatefulGreetingBindingConstructor name="John!" greeting="How are you doing?" />
             <StatefulGreetingBindingDefine name="John!" greeting="How are you doing?" />
